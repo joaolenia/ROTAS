58bb9ce5bfad1400ca5f2d26442499e4ba3e6ad3
Joao Pedro <joaopedrogolenia@gmail.com>
2026-09-17 15:24:26 -0300

    fix

--- rotas/src/App.tsx
+++ rotas/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Login from './componentes/Login'; // Certifique-se de que o caminho do import está correto
 import RouteSelection from './componentes/RouteSelection'; // Certifique-se de que o caminho do import está correto
 import './App.css'; // Mantenha se tiver estilos globais aqui, ou remova se não for usar
 
